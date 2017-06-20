f2e3498287262b0e477b536c986f62e0aecb9eb7
Felipez-Maturana <felipe.maturana.g@usach.cl>
2017-06-19 20:27:01 -0400

    form usuario

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,18 @@
 class PagesController < ApplicationController
   def x
+  	@users=User.all
+  end
+
+  def new_user
+  	if params[:nombre].present? && params[:edad].present? && params[:email].present?
+  		@user = User.new
+  		@user.nombre = params[:nombre]
+  		@user.email = params[:email]
+  		@user.edad = params[:edad]
+  		@user.save
+  		redirect_to pages_x_path, notice: "Usuario creado exitosamente"
+  	else 
+  		redirect_to pages_x_path, alert: "Ingrese todos los campos solicitados"
+  	end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root 'pages#x'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  post "pages/new_user"
 end
